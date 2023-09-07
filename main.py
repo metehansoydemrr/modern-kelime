4de7d75927c7efd95a3ee79c4fac0f11e4452815
metehansoydemrr <144052818+metehansoydemrr@users.noreply.github.com>
2023-09-07 18:31:25 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+print(Modern kelime sözlüğüne hoşgeldiniz)
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            ""
+            }
+            
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print (meme_dict[word])
+else:
+    print('Sözlükte böyle bir kelime yok ')
